6f7ef1b60fc4753c2cfac2aa40a8bcdf702fbbda
diego201grgtg <diego.delacruz.delahoz201@gmail.com>
2021-05-12 10:04:03 -0500

    fixed modlogs

--- commands/modlogs.js
+++ commands/modlogs.js
@@ -7,8 +7,7 @@ exports.run = async (client, message, args) => {
   const em = client.em;
   const yessir = client.yessir;
 
-  const target =
-    message.mentions.users.first() || (await client.users.fetch(args[0]));
+  const target = message.mentions.users.first();
   const makeID = client.makeID;
 
   if (!target)
