08b5667aeffed7589323d6d99113b2eab9ae12bc
diego201grgtg <diego.delacruz.delahoz201@gmail.com>
2021-05-13 18:05:56 -0500

    fixed nick

--- commands/nick.js
+++ commands/nick.js
@@ -5,26 +5,24 @@ exports.run = async (client, message, args) => {
   const em = client.em;
   const yessir = client.yessir;
   const ml = client.setModlog;
+
   const target =
     message.mentions.members.first() ||
     (await message.guild.members.fetch(args[0]));
   let nick = "";
-  let id = target.id;
-  if (id === message.author.id)
-    return message.channel.send(
-      em(`Failure!`, `You can't edit your nickname, dummy!`)
-    );
-  if (!target.manageable)
-    return message.channel.send(
-      em(`Failure!`, `I can't edit that user's nickname.`)
-    );
+  args.map((value, index) => {
+    if (index !== 0) nick += value + " ";
+  });
   target.setNickname(nick);
-  message.channel.send(
-    em(
-      `Success!`,
-      `<a:yes:836302807485251674> Set ${target}'s nickname to ${nick}`
-    )
-  );
+  const success = new MessageEmbed()
+    .setTitle("Sucess!")
+    .setColor("GREEN")
+    .setDescription(`Succesfully changed ${target}'s username to ${nick}!`)
+    .setFooter(`yay`);
+  message.channel.send(success).catch((err) => {
+    if (err) message.channel.send("**Something went wrong**");
+  });
+
   const userId = target.id;
   const guildId = message.guild.id;
   let modlog = {
@@ -52,7 +50,7 @@ exports.data = {
   botMode: 1, // same as above. Set it to 0 to require all perms to be fulfilled.
   minArgs: 2,
   maxArgs: null,
-  noDel: false, // change this to true if the command belongs to the "Moderation" category
+  noDel: true, // change this to true if the command belongs to the "Moderation" category
 }; // and you don't want to og message to be deleted.
 
 exports.errors = {
--- commands/userinfo.js
+++ commands/userinfo.js
@@ -1,32 +1,120 @@
 const Discord = require("discord.js");
 const { MessageEmbed } = require("discord.js");
+const moment = require("moment");
 
 exports.run = async (client, message, args) => {
   const em = client.em;
   const yessir = client.yessir;
 
-  const guild = message.guild;
-  const usr = message.mentions.users.first() || message.author;
-
   const member =
-    message.mentions.members.first() ||
-    (await message.guild.members.fetch(args[0]));
+    message.mentions.members.last() ||
+    message.guild.members.cache.get(args[0]) ||
+    message.member;
 
-  const user = member.user;
+  const trimArray = (arr, maxLen = 10) => {
+    if (arr.length > maxLen) {
+      const len = arr.length - maxLen;
+      arr = arr.slice(0, maxLen);
+      arr.push(` and ${len} more roles...`);
+    }
+    return arr;
+  };
 
-  const embed = new Discord.MessageEmbed()
-    .setAuthor(`${usr.tag}`, `${usr.displayAvatarURL({ dynamic: true })}`)
-    .setThumbnail(`${usr.displayAvatarURL({ dynamic: true })}`)
-    .setDescription(`${usr}'s Information`)
-    .addField(`**ID:**`, `${usr.id}`)
-    .addField(
-      `**Nickname:**`,
-      `${member.nickname || `**Cannot Find A Nickname For This User**`}`
+  const upperCase = (str) => {
+    return str.toUpperCase().replace(/_/g, " ").split(" ").join(" ");
+  };
+
+  const titleCase = (str) => {
+    return str
+      .toLowerCase()
+      .split(" ")
+      .map((word) => `${word.charAt(0).toUpperCase()}${word.slice(1)}`)
+      .join(" ");
+  };
+
+  const roles = member.roles.cache
+    .sort((a, b) => b.position - a.position)
+    .map((role) => role.toString())
+    .slice(0, -1);
+
+  let userFlags;
+  if (member.user.flags === null) {
+    userFlags = "";
+  } else {
+    userFlags = member.user.flags.toArray();
+  }
+  if (member.user.presence.status == "offline") {
+    userDevice = "";
+  } else if (!member.user.bot) {
+    userDevice = [Object.keys(member.user.presence.clientStatus)[0]];
+  } else if (member.user.bot) {
+    userDevice = "";
+  }
+  if (!member.user.bot) {
+    userInfo = "No";
+  } else if (member.user.bot) {
+    userInfo = "Yes";
+  }
+  if (member.user.presence.status == "dnd") {
+    status = "DND";
+  } else status = titleCase(member.user.presence.status);
+
+  const embed = new MessageEmbed()
+    .setAuthor(
+      `${member.user.tag} ${userDevice}`,
+      member.user.displayAvatarURL({ dynamic: true, size: 512 })
     )
-    .addField(`**Joined Server:**`, `${member.joinedAt}`)
-    .addField(`**Joined Discord:**`, `${usr.createdAt}`)
-    .addField(`**Status:**`, `${user.presence.status}`)
-    .setColor("RANDOM");
+    .setThumbnail(member.user.displayAvatarURL({ dynamic: true, size: 512 }))
+    .addFields(
+      {
+        name: "Joined Discord",
+        value: `${moment(member.user.createdTimestamp).format("DD MMM YYYY")}`,
+        inline: true,
+      },
+      {
+        name: "Joined Server",
+        value: `${moment(member.joinedAt).format("DD MMM YYYY")}`,
+        inline: true,
+      },
+      {
+        name: "Nickname",
+        value: `${member.displayName}` || "None",
+        inline: true,
+      },
+      {
+        name: "Discriminator",
+        value: `${member.user.discriminator}`,
+        inline: true,
+      },
+      { name: "Bot", value: `${userInfo}`, inline: true },
+      {
+        name: "User Colour",
+        value: `${upperCase(member.displayHexColor)}`,
+        inline: true,
+      },
+      { name: "User ID", value: `${member.user.id}`, inline: true },
+      {
+        name: "Highest Role",
+        value: `${
+          member.roles.highest.id === message.guild.id
+            ? "None"
+            : member.roles.highest
+        }`,
+        inline: true,
+      },
+      {
+        name: "Roles",
+        value: `${
+          roles.length < 10
+            ? roles.join(", ")
+            : roles.length > 10
+            ? trimArray(roles).join(", ")
+            : "None"
+        }`,
+        inline: false,
+      }
+    )
+    .setColor(`${member.displayHexColor || RANDOM}`);
   message.channel.send(embed);
 };
 
