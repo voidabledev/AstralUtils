eb8a6160f4e1551fd04b7834ebee5bdc12f53f72
voidable <diego.delacruz.delahoz201@gmail.com>
2021-05-31 17:04:32 -0500

    added cas.js

---
+++ commands/utilities/cas.js
@@ -0,0 +1,40 @@
+/* eslint-disable no-unused-vars */
+const alias = require('../../json/aliases.json');
+const successEmbed = require('../../functions/success-embed');
+const failureEmbed = require('../../functions/failure-embed');
+
+module.exports = {
+	help: {
+		name: 'cas',
+		description: 'Calls all staff',
+		usage: '',
+		aliases: alias.utilities.cas,
+		category: 'utilities',
+		cooldown: 7200,
+	},
+	data: {
+		minArgs: 0,
+		maxArgs: null,
+		userPerms: [],
+		botPerms: [],
+		requiredRoles: [],
+		delete: false,
+	},
+	async execute(message, args, client) {
+		message.channel.send('Are you sure you want to ping ALL the staff members?\n**Note:** This would be only usable in Raids or something like that.').then(async (msg) => {
+			await message.channel.awaitMessages((m) => m.author.id === message.author.id, {
+				max: 1,
+				time: 60000,
+				errors: ['time'],
+			})
+				.then(async (m) => {
+					if (m.first().content.toLowerCase().includes('yes')) {
+						message.channel.send('<@831996404549419018>');
+					}
+					else {
+						message.channel.send('Call-all-staff canceled.');
+					}
+				});
+		});
+	},
+};
