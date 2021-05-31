cda2fde7805e214828fada357604793eb010b19b
voidable <diego.delacruz.delahoz201@gmail.com>
2021-05-31 16:40:53 -0500

    yes

--- functions/error-embed.js
+++ functions/error-embed.js
@@ -2,8 +2,7 @@ const { MessageEmbed } = require('discord.js');
 function errorEmbed(errText) {
 	return new MessageEmbed()
 		.setColor('RED')
-		.setTitle('Uh oh...')
-		.setDescription(`There's been an error: \`\`\`\n${errText}\n\`\`\``)
+		.setDescription(`<a:error:849037573912657932> There's been an error: \`\`\`\n${errText}\n\`\`\``)
 		.setFooter('Please contact a developer if the error still persists')
 		.setTimestamp();
 }
