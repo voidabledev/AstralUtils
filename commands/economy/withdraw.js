4d04eb6eece5be6a65e7d81423302d944065b910
Aure <casimirusenormus@hotmail.de>
2021-06-22 15:54:37 +0200

    fix dep and with

--- commands/economy/withdraw.js
+++ commands/economy/withdraw.js
@@ -27,8 +27,8 @@ module.exports = {
 		if (!profile) {
 			return message.channel.send(failureEmbed('You don\'t have any money on your bank account dummy!'));
 		}
-		const amount = !isNaN(args[1]) ? parseInt(args[1]) :
-			!isNaN(args[1].slice(0, -1)) && args[1].slice(-1) === 'k' ? 1000 * parseInt(args[1].slice(0, -1)) :
+		const amount = !isNaN(args[0]) ? parseInt(args[0]) :
+			!isNaN(args[0].slice(0, -1)) && args[0].slice(-1) === 'k' ? 1000 * parseInt(args[0].slice(0, -1)) :
 				0;
 		if (!amount) {
 			return message.channel.send(failureEmbed('Try again, but this time tell me how much to withdraw.'));
