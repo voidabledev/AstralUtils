8def9dcd77876ddac957d1df8041b7b52a38cf08
Aure <casimirusenormus@hotmail.de>
2021-08-05 08:18:58 +0200

    avoid verification channel, so avoid getting quarantined

--- events/message.js
+++ events/message.js
@@ -13,7 +13,7 @@ module.exports = {
 	name: 'message',
 	once: false,
 	async execute(message, client) {
-		if(message.author.bot || !conf.guilds.includes(message.guild?.id)) return;
+		if(message.author.bot || !conf.guilds.includes(message.guild?.id) || message.channel.id === '872470276946952202') return;
 		afk(message);
 		automod(message, client);
 		const { cooldowns } = client;
