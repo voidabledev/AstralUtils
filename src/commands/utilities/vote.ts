2a89957cedfa13a8a613f4810b2faf820fb1c47a
Aure <casimirusenormus@hotmail.de>
2021-09-29 14:30:10 +0200

    buttons for vote, duh

--- src/commands/utilities/vote.ts
+++ src/commands/utilities/vote.ts
@@ -1,16 +1,26 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { Command } from '../../typings/command';
-import { MessageEmbed } from 'discord.js';
+import { MessageActionRow, MessageButton, MessageEmbed } from 'discord.js';
 
 export const command: Command = {
 	name: 'vote',
 	description: 'Get a link to this server\'s voting page.',
 	async run(interaction, options, client) {
 		const embed = new MessageEmbed()
-			.setTitle(`Vote for ${interaction.guild?.name}`)
-			.setDescription(`> Vote for **${interaction.guild?.name}** [here](https://top.gg/servers/831995980097388604/vote)`);
+			.setDescription(`> Vote for **${interaction.guild?.name}**`)
+			.setColor('GREEN');
+		const row = new MessageActionRow({
+			components: [
+				new MessageButton({
+					style: 'LINK',
+					url: 'https://top.gg/servers/831995980097388604/vote',
+				}),
+			],
+		});
 		await interaction.reply({
 			embeds: [embed],
+			components: [row],
+			ephemeral: true,
 		});
 	},
 };
